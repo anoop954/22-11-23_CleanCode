3d456cbb3e2436f338cc2a70b122571ed9966680
Anoop Godara <89520697+anoop954@users.noreply.github.com>
2023-11-22 11:17:42 +0530

    Update Day1_M_Name.cs

--- Day1_M_Name.cs
+++ Day1_M_Name.cs
@@ -1,1 +1,88 @@
+  Review1.cs
+int d; // elapsed time in days
 
+int elapsedDays;
+
+
+  Review2.cs
+var dataFromDb = db.GetFromService().ToList(); // Get List of employees
+
+var listOfEmployees = _DataBaseService.GetListOfEmployees().ToList();
+
+
+  Review3.cs
+int iCounter;
+string strFullName;
+DateTime dModifiedDate;
+
+int counter;
+string fullName;
+DateTime modifiedDate;
+
+
+  Review4.cs
+public bool IsShopOpen(string pDay, int pAmount)
+{
+    // some logic
+}
+
+public bool IsShopOpen(string day, int amount)
+{
+    // some logic
+}
+
+
+  Review5.cs
+const int DAYS_IN_WEEK = 7;
+const int daysInMonth = 30;
+
+var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+var Artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+
+bool EraseDatabase() {}
+bool Restore_database() {}
+
+class animal {}
+class Alpaca {}
+
+const int DAYS_IN_WEEK = 7;
+const int DAYS_IN_MONTH = 30;
+
+var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+var artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+
+bool EraseDatabase() {}
+bool RestoreDatabase() {}
+
+class Animal {}
+class Alpaca {}
+
+
+  Review6.cs
+public class Employee
+{
+    public Datetime sWorkDate { get; set; } // get set Start Working Date
+    public Datetime modTime { get; set; } // get set Modification Time
+}
+
+public class Employee
+{
+    public Datetime WorkDate { get; set; } // get set Start Working Date
+    public Datetime ModTime { get; set; } // get set Modification Time
+}
+
+
+  Review7.cs
+var employeephone;
+
+public double CalculateSalary(int workingdays, int workinghours)
+{
+    // some logic
+}
+
+var employeePhone;
+
+public double CalculateSalary(int workingDays, int workingHours)
+{
+    // some logic
+}
